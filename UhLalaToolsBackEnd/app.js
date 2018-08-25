6c5a779dc1c1f10fcd76727640dd9bbb627647c1
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2018-08-24 23:08:41 -0500

    yes

--- UhLalaToolsBackEnd/app.js
+++ UhLalaToolsBackEnd/app.js
@@ -12,11 +12,12 @@ mongoose.connect('mongodb://localhost:27017/uhlala-api');
 app.use(cors());
 
 var applicationSchema = new Schema({
+    endToEndTestsSchema: [endToEndTestSchema],
     name: { type: String, required: true },
     description: String
 });
 
-var testSchema = Schema({
+var endToEndTestSchema = Schema({
 	application: { type: Schema.Types.ObjectId, ref: 'Application' },
   	name: { type: String, required: true },
   	description: String,
@@ -24,7 +25,7 @@ var testSchema = Schema({
 });
 
 var Application = mongoose.model('Application', applicationSchema);
-var Test = mongoose.model('Test', testSchema);
+var EndToEndTest = mongoose.model('EndToEndTest', endToEndTestSchema);
 
 app.get('/', function(req, res) {
     res.send('api uhlala tools');
@@ -47,7 +48,7 @@ app.get('/applications/:id', function(req, res) {
 app.post('/applications', function(req, res) {
 	var application = new Application();
     application.name = req.body.name;
-    application.description = req.body.description || 'No hay descripcion';
+    application.description = req.body.description;
 
     application.save(function(error, savedApp) {
         if (error) return res.status(500).send(error);
@@ -58,51 +59,33 @@ app.post('/applications', function(req, res) {
 app.delete('/applications/:id', function(req, res) {
     Application.findById(req.params.id, function(error, application) {
         if (error) return res.status(500).send(error);
-
         application.remove(function(removingError) {
            if (removingError) return res.status(500).send({ error: removingError });
-
            res.json({ message: "Aplicacion removida exitosamente" });
         });
     });
 });
 
-app.get('/tests', function(req, res) {
-    Test.find({}, function(error, tests) {
-       if (error) return res.status(500).send(error);
-       res.json(tests);
-    });
-});
-
-app.post('/tests/:applicationId', function(req, res) {
-	var test = new Test({application:req.params.applicationId, name: req.body.name, description: req.body.description, type: req.body.type});
+app.post('/applications/:applicationId/end-to-end-tests', function(req, res) {
+	var test = new EndToEndTest({application: req.params.applicationId, name: req.body.name, description: req.body.description, type: req.body.type});
     test.save(function(error, savedTest) {
    		if (error) return res.status(500).send(error);
         res.json(savedApp);
     });
 });
 
-app.get('/tests/:id', function(req, res) {
-    Test.findById(req.params.id, function(error, test) {
-       if (error) return res.status(500).send(error);
-       res.json(test);
-    });
-});
-
-app.get('/applications/:applicationId/tests', function(req, res) {
-    Test.find({'application': req.params.applicationId}, function(error, tests) {
+app.get('/applications/:applicationId/end-to-end-tests', function(req, res) {
+    EndToEndTest.find({'application': req.params.applicationId}, function(error, tests) {
        if (error) return res.status(500).send(error);
        res.json(tests);
     });
 });
 
-app.delete('/tests/:id', function(req, res) {
-    Test.findById(req.params.id, function(error, test) {
+app.delete('/end-to-end-tests/:id', function(req, res) {
+    EndToEndTest.findById(req.params.id, function(error, test) {
         if (error) return res.status(500).send(error);
-
         test.remove(function(removingError) {
            if (removingError) return res.status(500).send({ error: removingError });
-
            res.json({ message: "Test removido exitosamente" });
         });
     });
