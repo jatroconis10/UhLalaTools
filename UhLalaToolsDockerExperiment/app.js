737074d9d70b9fc1a49605a81e7466a3c57d0009
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2018-10-09 23:26:27 -0500

    Avance dockers

--- UhLalaToolsDockerExperiment/app.js
+++ UhLalaToolsDockerExperiment/app.js
@@ -1,36 +1,55 @@
-/* jslint node: true */
-
 'use strict';
 
 require('dotenv').load();
 
-var shell = require('shelljs');
+const shell = require('shelljs');
+const path = require('path');
+const fs = require('fs');
+const AWS = require('aws-sdk');
 
-function copyTestsFiles() {
-    var fs = require('fs');
-    var AWS = require('aws-sdk');
-    var s3 = new AWS.S3({ accessKeyId: process.env.AWS_ACCESS_KEY, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY });
-    var appId = process.env.APP_ID;
-    var s3params = { Bucket: process.env.AWS_BUCKET, Delimiter: '/', Prefix: 'tests/' + appId + '/' };
+(async () => {
+    const s3 = new AWS.S3({ accessKeyId: process.env.AWS_ACCESS_KEY, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY });
+    const appId = process.env.APP_ID;
 
-    s3.listObjectsV2(s3params, function (_, data) {
-        data.Contents.forEach(function(file) {
-            if (!file.Key.endsWith('/')) {
-                var filenameSplit = file.Key.split('/');
-                var filename = filenameSplit[filenameSplit.length - 1];
-                var fileDestination = fs.createWriteStream('./tests/' + filename);
-                s3.getObject({ Bucket: process.env.AWS_BUCKET, Key: file.Key }).createReadStream().pipe(fileDestination);
+    function copyTests() {
+        return s3.listObjectsV2({ Bucket: process.env.AWS_BUCKET, Delimiter: '/', Prefix: `tests/${appId}/`}).promise().then((data) => {
+            const promises = data.Contents.filter((file) => !file.Key.endsWith('/')).map((file) => {
+                const filenameSplit = file.Key.split('/');
+                const filename = filenameSplit[filenameSplit.length - 1];
+                const fileDestination = fs.createWriteStream(`./tests/${filename}`);
+                return new Promise((resolve) => {
+                    s3.getObject({ Bucket: process.env.AWS_BUCKET, Key: file.Key }).createReadStream().pipe(fileDestination).on('finish', resolve);
+                });
+            });
+            return Promise.all(promises);
+        });
+    }
+
+    const copyTestsPromise = copyTests();
+    await copyTestsPromise;
+
+    const testsDirectory = 'tests'
+    const reportsDirectory = './tests/reports';
+    shell.mkdir(reportsDirectory);
+    if (shell.test('-d', testsDirectory) && shell.ls(testsDirectory).length !== 0) {
+        const wdio = path.normalize('./node_modules/.bin/wdio');
+        const conf = path.normalize(`${testsDirectory}/wdio.conf.js`);
+
+        shell.exec(`${wdio} ${conf}`);
+        fs.readdir(reportsDirectory, (_, files) => {
+            if (files) {
+                const reportsFolderPath = path.join(__dirname, reportsDirectory);
+                files.forEach(file => {
+                    const filePath = path.join(reportsFolderPath, file);
+                    if (!fs.lstatSync(filePath).isDirectory()) {
+                        fs.readFile(filePath, (_, fileContent) => {
+                            s3.putObject({ Bucket: process.env.AWS_BUCKET, Key: `tests/${appId}/reports/${file}`, Body: fileContent });
+                        });
+                    }
+                });
             }
         });
-    });
-}
-copyTestsFiles();
+    }
 
-var mongoose = require('mongoose');
-mongoose.connect(process.env.DB_URL);
-
-var db = mongoose.connection;
-
-// TODO Cambiar APP ID por ID de suite de pruebas
-
-
+    // TODO Reportar ejecución al test suite.
+})();
