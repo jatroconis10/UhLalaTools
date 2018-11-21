8832e4fd5cf7bc3f0a42bcab5929c6466c267491
Javier Troconis <ja.troconis10@uniandes.edu.co>
2018-11-20 19:46:12 -0500

    Added version supports for mobile tests

--- UhLalaToolsDeviceManager/routers/tests/mutation.js
+++ UhLalaToolsDeviceManager/routers/tests/mutation.js
@@ -16,24 +16,24 @@ const mutants = path.normalize(`${process.cwd()}/mutants`);
 
 var router = express.Router();
 
-router.get('/:appId/mutants', (req, res) => {
-    const appId = req.params.appId;
+router.get('/:appId/version/:version/mutants', (req, res) => {
+    const {appId, version } = req.params;
     
-    let currentMutants = mutationUtils.getMutants(appId);
+    let currentMutants = mutationUtils.getMutants(appId, version);
     res.json(currentMutants)
 })
 
-router.get('/:appId/reports', (req, res) => {
-    const appId = req.params.appId;
+router.get('/:appId/version/:version/reports', (req, res) => {
+    const {appId, version} = req.params;
 
-    let reports = reportutils.getAvailableReports(appId, 'mutants')
+    let reports = reportutils.getAvailableReports(appId, version, 'mutants')
     res.json(reports)
 })
 
-router.get('/:appId/reports/:timestampId', (req, res) => {
-    const { appId, timestampId } = req.params;
+router.get('/:appId/version/:version/reports/:timestampId', (req, res) => {
+    const { appId, version, timestampId } = req.params;
     
-    let report = reportutils.getReport(appId, 'mutants', timestampId)
+    let report = reportutils.getReport(appId, version, 'mutants', timestampId)
     if(!report.error) {
         res.json(report)
     } else {
@@ -41,10 +41,10 @@ router.get('/:appId/reports/:timestampId', (req, res) => {
     }
 })
 
-router.get('/:appId/mutants/:name', (req, res) => {
-    const appId = req.params.appId;
+router.get('/:appId/version/:version/mutants/:name', (req, res) => {
+    const {appId, version} = req.params;
     const name = req.params.name;
-    let specificMutant = path.normalize(`${mutants}/${appId}/mainSources/${name}`)
+    let specificMutant = path.normalize(`${mutants}/${appId}/${version}/mainSources/${name}`)
 
     res.setHeader('Content-Type', 'application/zip');
     res.attachment(`${name}.zip`)
@@ -56,32 +56,33 @@ router.get('/:appId/mutants/:name', (req, res) => {
     zip.directory(specificMutant, `/`).finalize();
 })
 
-router.post('/:appId/mutate', (req, res) => {
-    const appId = req.params.appId;
+router.post('/:appId/version/:version/mutate', (req, res) => {
+    const {appId, version} = req.params;
     let { package, gitUrl } = req.body
 
-    mutationUtils.mutate(appId, package, gitUrl)
+    mutationUtils.mutate(appId, version, package, gitUrl)
         .then(result => console.log('Mutantion finished'))
     res.json({message: 'Mutating ' + package })
 })
 
 
-router.post('/:appId/run', async (req, res) => {
+router.post('/:appId/version/:version/run', async (req, res) => {
     const { avds } = req.body;
-    const appId = req.params.appId;
+    const {appId, version} = req.params;
 
     res.json({message: 'Running mutants'});
-    const results = await mutationUtils.runMutants(appId, avds);
+    const results = await mutationUtils.runMutants(appId, version, avds);
 })
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        const appPath = path.normalize(`${apps}/${req.params.appId}`)
+        const { appId, version } = req.params;
+        const appPath = path.normalize(`${apps}/${appId}/${version}`)
         shell.mkdir('-p', appPath)
         cb(null, appPath);
     },
     filename: function (req, file, cb) {
-        cb(null, `${req.params.appId}.zip`);
+        cb(null, `${req.params.version}.zip`);
     }
 });
 
@@ -89,9 +90,10 @@ var upload = multer({
     storage: storage
 });
 
-router.post('/:appId', upload.single('app'), (req, res) => {
-    const appPath = path.normalize(`${apps}/${req.params.appId}`)
-    const testZip = path.normalize(`${appPath}/${req.params.appId}.zip`)
+router.post('/:appId/version/:version', upload.single('app'), (req, res) => {
+    const {appId, version} = req.params;
+    const appPath = path.normalize(`${apps}/${appId}/${version}`)
+    const testZip = path.normalize(`${appPath}/${version}.zip`)
     fs.createReadStream(testZip).pipe(unzip.Extract({ path: appPath }));
     shell.rm(testZip);
     res.send('File uploaded');
