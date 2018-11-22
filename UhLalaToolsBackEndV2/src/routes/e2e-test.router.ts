b8174236e75091368f4b045e59e116faf344a817
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2018-11-22 11:01:56 -0500

    servicios iniciales e2e

--- UhLalaToolsBackEndV2/src/routes/e2e-test.router.ts
+++ UhLalaToolsBackEndV2/src/routes/e2e-test.router.ts
@@ -4,6 +4,10 @@ import { E2ETestsController } from '../controllers';
 
 const router: Router = Router();
 
+router.get('/', E2ETestsController.getE2ETests);
 
+router.get('/:id', E2ETestsController.getE2ETest);
+
+router.post('/', E2ETestsController.createE2ETest);
 
 export const e2eTestsRouter: Router = router;
